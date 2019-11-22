75790f50baffb924fce6001de570ff5efdd6dd4b
Alex <jordan35@qq.com>
2019-11-22 02:40:30 +0000

    Apply fixes from StyleCI

--- src/Stock.php
+++ src/Stock.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace Alexxd\StockData;
-
 
 use Alexxd\StockData\Exceptions\HttpException;
 use Alexxd\StockData\Exceptions\InvalidArgumentException;
@@ -11,6 +9,7 @@ use GuzzleHttp\Client;
 class Stock
 {
     protected $key;
+
     protected $guzzleOptions = [];
 
     public function __construct($key)
@@ -30,25 +29,25 @@ class Stock
 
     public function getIntradayStock($symbol, $interval = '5', $format = 'json', $output_size = 'compact')
     {
-        if (!\in_array((string)$interval, ['1', '5', '15', '30', '60'])) {
-            throw new InvalidArgumentException('Invalid interval: ' . $interval . '.(The following values are supported: 1, 5, 15, 30, 60)');
+        if (!\in_array((string) $interval, ['1', '5', '15', '30', '60'])) {
+            throw new InvalidArgumentException('Invalid interval: '.$interval.'.(The following values are supported: 1, 5, 15, 30, 60)');
         }
         if (!\in_array(\strtolower($format), ['csv', 'json'])) {
-            throw new InvalidArgumentException('Invalid response format: ' . $format . '.(The following values are supported: csv,json)');
+            throw new InvalidArgumentException('Invalid response format: '.$format.'.(The following values are supported: csv,json)');
         }
         if (!\in_array(\strtolower($output_size), ['compact', 'full'])) {
-            throw new InvalidArgumentException('Invalid output size: ' . $output_size . '.(The following values are supported: compact,full)');
+            throw new InvalidArgumentException('Invalid output size: '.$output_size.'.(The following values are supported: compact,full)');
         }
 
         $url = sprintf('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=%s&interval=%s&datatype=%s&outputsize=%s&apikey=%s',
-            $symbol, $interval . 'min', $format, $output_size, $this->key);
+            $symbol, $interval.'min', $format, $output_size, $this->key);
 
         try {
             $response = $this->getHttpClient()->get($url)->getBody()->getContents();
+
             return $response;
         } catch (\Exception $e) {
             throw new HttpException($e->getMessage(), $e->getCode(), $e);
         }
-
     }
 }
