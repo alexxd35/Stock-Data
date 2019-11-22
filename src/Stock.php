7fbb35e04a70f79c7ac76262faa85365941c57ee
Alex <jordan35@qq.com>
2019-11-22 14:46:32 +0800

    daily weekly monthly finished

--- src/Stock.php
+++ src/Stock.php
@@ -27,27 +27,112 @@ class Stock
         $this->guzzleOptions = $options;
     }
 
-    public function getIntradayStock($symbol, $interval = '5', $format = 'json', $output_size = 'compact')
+    public function stockFactory($data)
     {
-        if (!\in_array((string) $interval, ['1', '5', '15', '30', '60'])) {
-            throw new InvalidArgumentException('Invalid interval: '.$interval.'.(The following values are supported: 1, 5, 15, 30, 60)');
+        if (!is_array($data)) {
+            throw new InvalidArgumentException('Invalid data: ' . $data . '.(Data must be an array)');
         }
-        if (!\in_array(\strtolower($format), ['csv', 'json'])) {
-            throw new InvalidArgumentException('Invalid response format: '.$format.'.(The following values are supported: csv,json)');
+        foreach (['function', 'symbol'] as $value) {
+            if (!array_key_exists($value, $data)) {
+                throw new InvalidArgumentException('Array key: ' . $value . ' must be exists');
+            }
         }
-        if (!\in_array(\strtolower($output_size), ['compact', 'full'])) {
-            throw new InvalidArgumentException('Invalid output size: '.$output_size.'.(The following values are supported: compact,full)');
+        $url = 'https://www.alphavantage.co/query';
+        $query = array_filter([
+            'function' => strtoupper($data['function']),
+            'symbol' => $data['symbol'],
+            'datatype' => $data['format'],
+            'outputsize' => $data['output_size'],
+            'apikey' => $this->key
+        ]);
+        if ('TIME_SERIES_INTRADAY' === strtoupper($data['function'])) {
+            if (!array_key_exists('interval', $data)) {
+                throw new InvalidArgumentException('Array key: interval must be exists in this function');
+            }
+            $query['interval'] = $data['interval'] . 'min';
         }
-
-        $url = sprintf('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=%s&interval=%s&datatype=%s&outputsize=%s&apikey=%s',
-            $symbol, $interval.'min', $format, $output_size, $this->key);
-
         try {
-            $response = $this->getHttpClient()->get($url)->getBody()->getContents();
+            $response = $this->getHttpClient()->get($url, [
+                'query' => $query,
+            ])->getBody()->getContents();
 
             return $response;
         } catch (\Exception $e) {
             throw new HttpException($e->getMessage(), $e->getCode(), $e);
         }
+
+
+    }
+
+    public function getIntradayStock($symbol, $interval = '5', $format = 'json', $output_size = 'compact')
+    {
+        if (!\in_array((string)$interval, ['1', '5', '15', '30', '60'])) {
+            throw new InvalidArgumentException('Invalid interval: ' . $interval . '.(The following values are supported: 1, 5, 15, 30, 60)');
+        }
+        if (!\in_array(\strtolower($format), ['csv', 'json'])) {
+            throw new InvalidArgumentException('Invalid response format: ' . $format . '.(The following values are supported: csv,json)');
+        }
+        if (!\in_array(\strtolower($output_size), ['compact', 'full'])) {
+            throw new InvalidArgumentException('Invalid output size: ' . $output_size . '.(The following values are supported: compact,full)');
+        }
+        $data = [
+            'function' => 'TIME_SERIES_INTRADAY',
+            'symbol' => $symbol,
+            'interval' => $interval,
+            'format' => $format,
+            'output_size' => $output_size
+        ];
+        return $this->stockFactory($data);
+    }
+
+    public function getDailyStock($symbol, $format = 'json', $output_size = 'compact')
+    {
+        if (!\in_array(\strtolower($format), ['csv', 'json'])) {
+            throw new InvalidArgumentException('Invalid response format: ' . $format . '.(The following values are supported: csv,json)');
+        }
+        if (!\in_array(\strtolower($output_size), ['compact', 'full'])) {
+            throw new InvalidArgumentException('Invalid output size: ' . $output_size . '.(The following values are supported: compact,full)');
+        }
+        $data = [
+            'function' => 'TIME_SERIES_DAILY',
+            'symbol' => $symbol,
+            'format' => $format,
+            'output_size' => $output_size
+        ];
+        return $this->stockFactory($data);
+    }
+
+    public function getWeeklyStock($symbol, $format = 'json', $output_size = 'compact')
+    {
+        if (!\in_array(\strtolower($format), ['csv', 'json'])) {
+            throw new InvalidArgumentException('Invalid response format: ' . $format . '.(The following values are supported: csv,json)');
+        }
+        if (!\in_array(\strtolower($output_size), ['compact', 'full'])) {
+            throw new InvalidArgumentException('Invalid output size: ' . $output_size . '.(The following values are supported: compact,full)');
+        }
+        $data = [
+            'function' => 'TIME_SERIES_WEEKLY',
+            'symbol' => $symbol,
+            'format' => $format,
+            'output_size' => $output_size
+        ];
+        return $this->stockFactory($data);
+    }
+
+    public function getMonthlyStock($symbol, $format = 'json', $output_size = 'compact')
+    {
+        if (!\in_array(\strtolower($format), ['csv', 'json'])) {
+            throw new InvalidArgumentException('Invalid response format: ' . $format . '.(The following values are supported: csv,json)');
+        }
+        if (!\in_array(\strtolower($output_size), ['compact', 'full'])) {
+            throw new InvalidArgumentException('Invalid output size: ' . $output_size . '.(The following values are supported: compact,full)');
+        }
+        $data = [
+            'function' => 'TIME_SERIES_MONTHLY',
+            'symbol' => $symbol,
+            'format' => $format,
+            'output_size' => $output_size
+        ];
+        return $this->stockFactory($data);
     }
 }
