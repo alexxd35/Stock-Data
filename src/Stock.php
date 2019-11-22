f58718143bcc2717bc6ed61c0001d452338d9367
Alex <jordan35@qq.com>
2019-11-22 06:46:44 +0000

    Apply fixes from StyleCI

--- src/Stock.php
+++ src/Stock.php
@@ -30,11 +30,11 @@ class Stock
     public function stockFactory($data)
     {
         if (!is_array($data)) {
-            throw new InvalidArgumentException('Invalid data: ' . $data . '.(Data must be an array)');
+            throw new InvalidArgumentException('Invalid data: '.$data.'.(Data must be an array)');
         }
         foreach (['function', 'symbol'] as $value) {
             if (!array_key_exists($value, $data)) {
-                throw new InvalidArgumentException('Array key: ' . $value . ' must be exists');
+                throw new InvalidArgumentException('Array key: '.$value.' must be exists');
             }
         }
         $url = 'https://www.alphavantage.co/query';
@@ -43,14 +43,15 @@ class Stock
             'symbol' => $data['symbol'],
             'datatype' => $data['format'],
             'outputsize' => $data['output_size'],
-            'apikey' => $this->key
+            'apikey' => $this->key,
         ]);
         if ('TIME_SERIES_INTRADAY' === strtoupper($data['function'])) {
             if (!array_key_exists('interval', $data)) {
                 throw new InvalidArgumentException('Array key: interval must be exists in this function');
             }
-            $query['interval'] = $data['interval'] . 'min';
+            $query['interval'] = $data['interval'].'min';
         }
+
         try {
             $response = $this->getHttpClient()->get($url, [
                 'query' => $query,
@@ -60,79 +61,81 @@ class Stock
         } catch (\Exception $e) {
             throw new HttpException($e->getMessage(), $e->getCode(), $e);
         }
-
-
     }
 
     public function getIntradayStock($symbol, $interval = '5', $format = 'json', $output_size = 'compact')
     {
-        if (!\in_array((string)$interval, ['1', '5', '15', '30', '60'])) {
-            throw new InvalidArgumentException('Invalid interval: ' . $interval . '.(The following values are supported: 1, 5, 15, 30, 60)');
+        if (!\in_array((string) $interval, ['1', '5', '15', '30', '60'])) {
+            throw new InvalidArgumentException('Invalid interval: '.$interval.'.(The following values are supported: 1, 5, 15, 30, 60)');
         }
         if (!\in_array(\strtolower($format), ['csv', 'json'])) {
-            throw new InvalidArgumentException('Invalid response format: ' . $format . '.(The following values are supported: csv,json)');
+            throw new InvalidArgumentException('Invalid response format: '.$format.'.(The following values are supported: csv,json)');
         }
         if (!\in_array(\strtolower($output_size), ['compact', 'full'])) {
-            throw new InvalidArgumentException('Invalid output size: ' . $output_size . '.(The following values are supported: compact,full)');
+            throw new InvalidArgumentException('Invalid output size: '.$output_size.'.(The following values are supported: compact,full)');
         }
         $data = [
             'function' => 'TIME_SERIES_INTRADAY',
             'symbol' => $symbol,
             'interval' => $interval,
             'format' => $format,
-            'output_size' => $output_size
+            'output_size' => $output_size,
         ];
+
         return $this->stockFactory($data);
     }
 
     public function getDailyStock($symbol, $format = 'json', $output_size = 'compact')
     {
         if (!\in_array(\strtolower($format), ['csv', 'json'])) {
-            throw new InvalidArgumentException('Invalid response format: ' . $format . '.(The following values are supported: csv,json)');
+            throw new InvalidArgumentException('Invalid response format: '.$format.'.(The following values are supported: csv,json)');
         }
         if (!\in_array(\strtolower($output_size), ['compact', 'full'])) {
-            throw new InvalidArgumentException('Invalid output size: ' . $output_size . '.(The following values are supported: compact,full)');
+            throw new InvalidArgumentException('Invalid output size: '.$output_size.'.(The following values are supported: compact,full)');
         }
         $data = [
             'function' => 'TIME_SERIES_DAILY',
             'symbol' => $symbol,
             'format' => $format,
-            'output_size' => $output_size
+            'output_size' => $output_size,
         ];
+
         return $this->stockFactory($data);
     }
 
     public function getWeeklyStock($symbol, $format = 'json', $output_size = 'compact')
     {
         if (!\in_array(\strtolower($format), ['csv', 'json'])) {
-            throw new InvalidArgumentException('Invalid response format: ' . $format . '.(The following values are supported: csv,json)');
+            throw new InvalidArgumentException('Invalid response format: '.$format.'.(The following values are supported: csv,json)');
         }
         if (!\in_array(\strtolower($output_size), ['compact', 'full'])) {
-            throw new InvalidArgumentException('Invalid output size: ' . $output_size . '.(The following values are supported: compact,full)');
+            throw new InvalidArgumentException('Invalid output size: '.$output_size.'.(The following values are supported: compact,full)');
         }
         $data = [
             'function' => 'TIME_SERIES_WEEKLY',
             'symbol' => $symbol,
             'format' => $format,
-            'output_size' => $output_size
+            'output_size' => $output_size,
         ];
+
         return $this->stockFactory($data);
     }
 
     public function getMonthlyStock($symbol, $format = 'json', $output_size = 'compact')
     {
         if (!\in_array(\strtolower($format), ['csv', 'json'])) {
-            throw new InvalidArgumentException('Invalid response format: ' . $format . '.(The following values are supported: csv,json)');
+            throw new InvalidArgumentException('Invalid response format: '.$format.'.(The following values are supported: csv,json)');
         }
         if (!\in_array(\strtolower($output_size), ['compact', 'full'])) {
-            throw new InvalidArgumentException('Invalid output size: ' . $output_size . '.(The following values are supported: compact,full)');
+            throw new InvalidArgumentException('Invalid output size: '.$output_size.'.(The following values are supported: compact,full)');
         }
         $data = [
             'function' => 'TIME_SERIES_MONTHLY',
             'symbol' => $symbol,
             'format' => $format,
-            'output_size' => $output_size
+            'output_size' => $output_size,
         ];
+
         return $this->stockFactory($data);
     }
 }
